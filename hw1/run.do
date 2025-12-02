04f403998059da5c196530aea8a40d5f994f695f
BossPSU <dboss@pdx.edu>
2025-12-01 19:36:42 -0800

    cleanup

--- hw1/run.do
+++
@@ -1,5 +0,0 @@
-vlog martian_days.sv martian_days_tb.sv
-vopt work.top -o top_opt +acc
-vsim top_opt
-add wave *
-run -all
--- hw1/run.tcl
+++
@@ -1,5 +0,0 @@
-needed to add lines 2 and 3 to run.do because objects were getting optimized out of the design
-
-run with below command, click "no" when asked "are you sure you want to finish"
-
-vsim -do run.do
